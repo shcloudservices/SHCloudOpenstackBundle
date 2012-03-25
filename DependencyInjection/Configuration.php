fdc7062ff49271cc51e931a0f2f7ee78513f2647
Adrian Moya <adrianmoya@gmail.com>
2012-03-24 21:01:14 -0430

    Playing with this

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,11 +5,6 @@ namespace SHCloud\OpenstackBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -20,10 +15,18 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('sh_cloud_openstack');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
+        $rootNode
+            ->children()
+                ->scalarNode('username')
+                    ->isRequired()
+                ->end()
+                ->scalarNode('password')
+                    ->isRequired()
+                ->end()
+                ->scalarNode('auth_url')
+                    ->isRequired()
+                ->end()
+            ->end();
         return $treeBuilder;
     }
 }
--- DependencyInjection/SHCloudOpenstackExtension.php
+++ DependencyInjection/SHCloudOpenstackExtension.php
@@ -7,22 +7,18 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
-class SHCloudOpenstackExtension extends Extension
-{
+class SHCloudOpenstackExtension extends Extension {
+
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+    public function load(array $configs, ContainerBuilder $container) {
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $config);
+
+        $loader = new Loader\XMLFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
     }
+
 }
